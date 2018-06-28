d6217ce3390bd611652d5bbee326202fe29ef9e7
konstantinpodmasko <podmasko.konstantin@gmail.com>
2018-06-28 16:39:54 +0300

    add sass, grid

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from "react"
 import TrainsList from "./TrainsList"
 import TrainsMap from "./TrainsMap"
+import "../sass/App.scss"
 
 const App = () => {
     return (
-        <div>
-            <TrainsMap/>
-            <TrainsList/>
+        <div className="App container">
+            <div className="row">
+                <div className="item-6">
+                    <TrainsMap/>
+                </div>
+                <div className="item-6">
+                    <TrainsList/>
+                </div>
+            </div>
         </div>
     )
 };
